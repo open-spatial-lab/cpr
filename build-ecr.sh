b2d62496f37886522087170a55b1599b32f47a91
nofurtherinformation <dhalpern@uchicago.edu>
2025-03-04 10:30:54 -0600

    clean up docker things

--- build-ecr.sh
+++ build-ecr.sh
@@ -2,7 +2,7 @@
 aws ecr get-login-password --region us-west-1 | docker login --username AWS --password-stdin 345225490760.dkr.ecr.us-west-1.amazonaws.com
 
 # Build and tag your image
-docker build -t pan-data-update .
+docker build --platform linux/amd64 -t pan-data-update .
 docker tag pan-data-update:latest 345225490760.dkr.ecr.us-west-1.amazonaws.com/pan-data-update:latest
 
 # Push to ECR
